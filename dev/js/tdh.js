dfd4df93d5bcdea7b4988ab0b6d7cf1799944181
Nicole <NicoleLaptop@gmx.de>
2016-04-05 15:21:22 +0200

    added comment

--- dev/js/tdh.js
+++ dev/js/tdh.js
@@ -1,4 +1,6 @@
 /* individuelles JS */
+
+/* JS for accordion */
 $(".accordion .entry").on("click", function() {
     $(".accordion .entry").removeClass("opened").attr("aria-hidden", "true");
     $(this).addClass("opened").attr("aria-hidden", "false");
